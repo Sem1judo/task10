eab8b29e5323a68f92393eef99542325da509260
Stanislav <judo1stss1@gmail.com>
2020-05-08 13:03:54 +0300

    add test

---
+++ src/test/java/com/ua/foxminded/task10/TimeTableTest.java
@@ -0,0 +1,12 @@
+package com.ua.foxminded.task10;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TimeTableTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
